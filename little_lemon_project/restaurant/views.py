aeb266afe7427765c7fe08c3edf72fd17bd1027a
Kirill777-web <ki.lazutin@gmail.com>
2024-04-29 16:37:47 -0300

    Defining URLs for the restaurant app Defining Models for the restaurant app Booking, Menu Defining Views for the restaurant app Register models in admin.py

--- little_lemon_project/restaurant/views.py
+++ little_lemon_project/restaurant/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, redirect
+from .forms import BookingForm
+from .models import Menu
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'book.html', context)
+
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', main_data)
+
+
+def display_menu_items(request, pk=None):
+    if pk:
+        try:
+            menu_item = Menu.objects.get(pk=pk)
+        except Menu.DoesNotExist:
+            raise Http404("Menu item does not exist")
+    else:
+        menu_item = ''
+    return render(request, 'menu_item.html', {'menu_item': menu_item})
