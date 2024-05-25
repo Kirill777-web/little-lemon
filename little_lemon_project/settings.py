980caea01ef5554c5f07f763d185e3f27454c4ca
Kirill777-web <ki.lazutin@gmail.com>
2024-05-25 19:51:45 -0300

    Prepare for Heroku deployment

--- little_lemon_project/settings.py
+++ little_lemon_project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,6 +123,8 @@ USE_TZ = True
 
 # The settings for static files have been updated for the Graded assessment
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'restaurant', 'static'),
