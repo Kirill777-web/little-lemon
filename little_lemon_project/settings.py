fc0fa19cede6960424c94ef4eeba2c6053eae192
Kirill777-web <ki.lazutin@gmail.com>
2024-05-26 15:54:55 -0300

    settings

--- little_lemon_project/settings.py
+++ little_lemon_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-vvsx!l6f&*jtm+6hy5tp=w$vx$y-*z)iom)=-1o5ewfer-41%l
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'little-lemon-app-ce2c04df10ca.herokuapp.com', '127.0.0.1']
+    'little-lemon-app-ce2c04df10ca.herokuapp.com']
 
 
 # Application definition
